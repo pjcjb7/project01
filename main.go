23606392be86abcd45a0799401939a5cf556528b
pjcjb7 <accessresidant7@gmail.com>
2024-05-03 06:47:57 +0300

    change mainindex.html location

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Define a route to serve HTML content
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Hello, Golang!",
+		})
+	})
+
+	// Run the server
+	r.Run(":8080")
+}
