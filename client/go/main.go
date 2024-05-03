8fd68eec7a046af237f9e7a4d5f85cc420862fcf
pjcjb7 <accessresidant7@gmail.com>
2024-05-03 06:08:21 +0300

    Add changes to main.go

--- client/go/main.go
+++ client/go/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"encoding/csv"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request){
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		// Parse form data
 		err := r.ParseForm()
 		if err != nil {
-			http.Error(w, "Failed to parse Form data", http.StatusInternalServerError)
+			http.Error(w, "Failed to parse form data", http.StatusInternalServerError)
 			return
 		}
 
@@ -19,25 +20,28 @@ func main() {
 		surname := r.Form.Get("surname")
 		email := r.Form.Get("email")
 		age := r.Form.Get("age")
-		password = r.Form.Get("password")
+		password := r.Form.Get("password")
 
-		//Write data to cvs File (Excel -Com[patible)
-		file,err :=os.OpenFile("form_data.cvs" , os.0_APPEND|os.0_CREATE|os.0_WRONLY, 0644)
+		// Write data to CSV file (Excel-compatible)
+		file, err := os.OpenFile("form_data.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
-			http.Error(w, "Failed to open file", http.StatusIntenalServerError)
+			http.Error(w, "Failed to open file", http.StatusInternalServerError)
 			return
 		}
 		defer file.Close()
 
-		writetr :=cvs.NewWriter(file)
+		writer := csv.NewWriter(file)
 		defer writer.Flush()
 
-		err =writer.Write([]string{name, email})
+		err = writer.Write([]string{firstName, surname, age, email, password})
 		if err != nil {
 			http.Error(w, "Failed to write data to file", http.StatusInternalServerError)
 			return
 		}
 
+		// Optionally, you can send a response back to the client
+		w.Write([]byte("Data saved successfully!"))
 	})
+
 	http.ListenAndServe(":8080", nil)
 }
